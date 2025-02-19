f72358089e7ed79dedaac808eb3790fbf7262d1c
raphaelgeronimo <raphaelgabrielgeronimo@gmail.com>
2025-02-20 00:55:48 +0800

    added a func to clear remember me in logout.php

--- Accounts/logout.php
+++ Accounts/logout.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
+
+// Destroy session
 session_unset();
 session_destroy();
-header("Location: ../homepage.php");
+
+// Clear "Remember Me" stored credentials
+echo "<script>
+    localStorage.removeItem('rememberedEmail');
+    localStorage.removeItem('rememberedPassword');
+    localStorage.removeItem('remembered');
+    window.location.href = '../../Accounts/loginverify/loginlogic.php';
+</script>";
 exit();
 ?>
